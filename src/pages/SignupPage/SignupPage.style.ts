85d4236190bfa31813c99d43d9fb9f84c50edf52
choi-hi <gmltmdsp@hanmail.net>
2025-05-12 15:42:55 +0900

    feat:  Signup 기능 및 UI 구현

---
+++ src/pages/SignupPage/SignupPage.style.ts
@@ -0,0 +1,19 @@
+export const signupPageWrapper =
+	"w-full min-h-screen flex justify-center items-center bg-bg-primary bg-cover bg-center bg-no-repeat overflow-auto";
+export const signupContainer =
+	"min-w-[350px] min-h-[500px] bg-bg-sub p-10 rounded-modal shadow-2 flex flex-col";
+
+export const signupTitle = "text-heading-h5 py-8";
+
+export const signupFormContainer = "flex flex-col w-full mt-9";
+
+export const signupSection = "text-sm text-center";
+
+export const signupButtonContainer = "h-1 mb-5";
+
+export const signupLinkButton =
+	"text-text-info cursor-pointer underline hover:opacity-60 transition";
+
+export const signupSectionContainer = "mt-10";
+
+export const errorMessage = "text-sm mt-8 text-center text-text-warning";
