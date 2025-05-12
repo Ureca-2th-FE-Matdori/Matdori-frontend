dda9e6e75e147eb6d79498120d1d6f739c39435d
choi-hi <gmltmdsp@hanmail.net>
2025-05-12 13:16:38 +0900

    feat: user 정보 전역 상태관리 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import { setUserId } from "./stores/userSlice";
 
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const storeId = sessionStorage.getItem("userId");
+		if (storeId) {
+			dispatch(setUserId(storeId));
+		}
+	}, []);
+
 	return (
 		<div>
-			<main>
-				<Outlet />
-			</main>
+			<Outlet />
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import AppRouter from "./AppRouter";
 import "./index.css";
+import { store } from "./stores";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<AppRouter />
+		<Provider store={store}>
+			<AppRouter />
+		</Provider>
 	</StrictMode>
 );
