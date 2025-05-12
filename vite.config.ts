5a60ae535ca7f19db4811fa1505564d470559eb7
gichulLimitLess <hanascreen@konkuk.ac.kr>
2025-05-12 14:58:10 +0900

    fix: 포트번호 변경
    - 포트번호 5173 -> 3000으로 재변경

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
 	plugins: [react(), tailwindcss(), tsconfigPaths()],
 	server: {
-		port: 5173,
+		port: 3000,
 	},
 });
