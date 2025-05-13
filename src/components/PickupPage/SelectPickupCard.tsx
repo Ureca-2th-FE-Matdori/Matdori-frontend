507501be862f7e26e3c991cc7bf3c1514ef27974
choi-hi <gmltmdsp@hanmail.net>
2025-05-13 15:47:04 +0900

    feat: 뽑기 창 선택하기 페이지 UI 제작

---
+++ src/components/PickupPage/SelectPickupCard.tsx
@@ -0,0 +1,38 @@
+import Lottie from "lottie-react";
+import { useIsMobile } from "@stores/IsMobileContext";
+import * as styles from "./SelectPickupCard.style";
+import Button from "../common/Button/Button";
+
+interface SelectPickupCardProps {
+	title: string;
+	animationData: object;
+	onClick: React.MouseEventHandler<HTMLButtonElement>;
+	buttonlabel: string;
+}
+
+const SelectPickupCard = ({
+	title,
+	animationData,
+	onClick,
+	buttonlabel,
+}: SelectPickupCardProps) => {
+	const isMobile = useIsMobile();
+	return (
+		<div className={styles.pickupCardContainer}>
+			<div className={styles.pickupCardTitle}>{title}</div>
+			{!isMobile && (
+				<Lottie
+					loop
+					animationData={animationData}
+					style={{ width: 300, height: 250 }}
+				/>
+			)}
+
+			<div className={styles.pickupButtonContainer}>
+				<Button size="sm" label={buttonlabel} onClick={onClick} />
+			</div>
+		</div>
+	);
+};
+
+export default SelectPickupCard;
